feb782e9727b55b838e2e57aa44fe225a1437585
rohittt-29 <rm2193352@gmail.com>
2025-09-30 13:16:17 +0530

    backend updated

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,4 +1,4 @@
 export const BASE_URL =
   import.meta.env.VITE_API_BASE ||
   import.meta.env.VITE_API_URL ||
-  (import.meta.env.PROD ? "https://brain-box-1.onrender.com" : "http://localhost:5555")
+  (import.meta.env.PROD ? "https://brain-box-w7mg.onrender.com" : "http://localhost:5555")
